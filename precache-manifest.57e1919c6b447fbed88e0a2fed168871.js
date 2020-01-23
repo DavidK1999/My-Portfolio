a98b6de033d690c2d7b67c1ac387146b5a86a403
DavidK1999 <davidkalina404@gmail.com>
2020-01-23 07:15:29 -0700

    Updates

---
+++ precache-manifest.57e1919c6b447fbed88e0a2fed168871.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11045fe748b15cc5fe403c08cf7b644c",
+    "url": "https//davidk1999.github.io/portfolio/index.html"
+  },
+  {
+    "revision": "3e8322959611050b71db",
+    "url": "https//davidk1999.github.io/portfolio/static/css/main.19611e6c.chunk.css"
+  },
+  {
+    "revision": "a84b357e850a07da0a4b",
+    "url": "https//davidk1999.github.io/portfolio/static/js/2.9118220e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "https//davidk1999.github.io/portfolio/static/js/2.9118220e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3e8322959611050b71db",
+    "url": "https//davidk1999.github.io/portfolio/static/js/main.700160b3.chunk.js"
+  },
+  {
+    "revision": "ef80db329ec403d8eb36",
+    "url": "https//davidk1999.github.io/portfolio/static/js/runtime-main.e11c4984.js"
+  },
+  {
+    "revision": "a27a53d0c6b5837a2c2f54ba1efd3e99",
+    "url": "https//davidk1999.github.io/portfolio/static/media/Block-Bolt.a27a53d0.png"
+  },
+  {
+    "revision": "884a71edad15554ce61e70a82e6b6148",
+    "url": "https//davidk1999.github.io/portfolio/static/media/Todoit.884a71ed.png"
+  },
+  {
+    "revision": "389757d8f1599ad9e9410413b810cdb7",
+    "url": "https//davidk1999.github.io/portfolio/static/media/mypc.389757d8.jpg"
+  },
+  {
+    "revision": "27f7768003d0d6bd6d75bcdf8162992f",
+    "url": "https//davidk1999.github.io/portfolio/static/media/profile.27f77680.jpg"
+  },
+  {
+    "revision": "b0f8163ee35245dc92a3ef62db2ed123",
+    "url": "https//davidk1999.github.io/portfolio/static/media/thisbetterwork.b0f8163e.png"
+  },
+  {
+    "revision": "0e37f86dd54c233863057822c29cdfe4",
+    "url": "https//davidk1999.github.io/portfolio/static/media/violin.0e37f86d.jpg"
+  }
+]);
