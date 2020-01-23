a5fc6f584c0fe9c92a04eb0ff034b618289c1876
DAVIDK-1999 <davidkalina1999@gmail.com>
2020-01-23 12:10:29 -0700

    Removed Pictures

--- src/App.js
+++ src/App.js
@@ -20,7 +20,6 @@ function App() {
       <ImageContainer images={[Profile]} alignment={"flex-start"}/>
       {/* <ImageContainer src={Profile} alignment={"flex-start"}/> */}
       <Text title={"My Hobbies"} body={"While coding is one of my biggest hobbies, I also possess many others. I love music. I played the violin for an extensive amount of time and dabble with the piano on occasion. Many of my hobbies revolve around tech. I'm a custom PC enthusiast. I like to regularly update my PC with the lastest and greatest specs to play the most demanding games. A recent hobby of mine has been playing ping pong. My cohort buddy and I will take advantage of any moment we get to play an intense match of ping pong."}/>
-      <ImageContainer images={[MyPC, Violin]} alignment={"space-between"}/>
       <Text title={"Skills"} img={<SkillSection skills={['HTML', 'CSS', 'Bootstrap', 'JavaScript', 'React', 'MongoDB', 'Python', 'SQL', 'Express.js']}/>}/>
       
       <Jumbo id={"projects"} title={"Projects"} text={"Some of the projects I've made"}/>
